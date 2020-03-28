9a4a7129c30a8f0c8365fe8b4a240daab900f99b
Christophe Villeger <villeger.c@gmail.com>
2020-03-28 05:46:28 +0100

    Replace Google Chart by Simple Table

--- script.js
+++ script.js
@@ -1,54 +1,23 @@
-function getDataByProvince(data, province) {
-    return data.filter(
-        function(data){ return data.province === province }
-    )[0].timeline;
-}
-
-function drawChart(content) {
-
-    let data = new google.visualization.DataTable();
-    data.addColumn('date', 'Date');
-    data.addColumn('number', 'Cas confirmés');
-    data.addColumn('number', 'Rétablis');
-    data.addColumn('number', 'Morts');
-
-    let length = Object.keys(content.cases).length;
-    for (let index = 0; index < length; index++) {
-        let keyCases = Object.keys(content.cases)[index];
-        let keyDeaths = Object.keys(content.deaths)[index];
-        let keyRecovered = Object.keys(content.recovered)[index];
-        data.addRow(
-            [new Date(keyCases), parseInt(content.cases[keyCases]), parseInt(content.recovered[keyRecovered]), parseInt(content.deaths[keyDeaths])]
-        );
-    }
-
-    var options = {
-        chart: {
-            title: 'Progression du COVID-19 en France',
-            subtitle: 'Source : corona.lmao.ninja'
-        },
-        width: 900,
-        height: 500
-    };
-
-    var chart = new google.charts.Line(document.getElementById('chart'));
-
-    chart.draw(data, google.charts.Line.convertOptions(options));
-}
-
+let httpRequest = new XMLHttpRequest();
 document.addEventListener("DOMContentLoaded", function() {
-    let httpRequest = new XMLHttpRequest();
+    let tbody = document.getElementsByTagName('tbody')[0];
     httpRequest.onreadystatechange = function() {
         if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-            let content = getDataByProvince(JSON.parse(httpRequest.response), 'france');
-            drawChart(content);
+            let content = JSON.parse(httpRequest.response).timeline;
+            let length = Object.keys(content.cases).length;
+
+            for (let index = 0; index < length; index++) {
+                let keyCases = Object.keys(content.cases)[index];
+                let keyDeaths = Object.keys(content.deaths)[index];
+                let html = '<tr>' +
+                    '<td>' + keyCases + '</td>' +
+                    '<td>' + content.cases[keyCases] + '</td>' +
+                    '<td>' + content.deaths[keyDeaths] + '</td>' +
+                    '</tr>';
+                tbody.innerHTML = html + tbody.innerHTML;
+            }
         }
     };
-    httpRequest.open("GET", 'https://corona.lmao.ninja/historical');
+    httpRequest.open("GET", 'https://corona.lmao.ninja/v2/historical/france');
     httpRequest.send();
-
-    google.charts.load('current', {'packages':['line']});
-
-
-
 });
